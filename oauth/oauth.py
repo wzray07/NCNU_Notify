f6ba3cab984670ae9153836c50c62a0b2cab4b78
Ray <dtjfhjtf@icloud.com>
2024-01-28 18:24:18 +0800

    path error fixed v0.1.2

--- oauth/oauth.py
+++ oauth/oauth.py
@@ -8,7 +8,7 @@ SECRET = "EzLJPlSm7wRYzdxHO4FNS8VbkEXANEFCcN3DE58BWgE"
 URI = "http://localhost:8080/callback/"
 lotify = Client(client_id=CLIENT_ID, client_secret=SECRET, redirect_uri=URI)
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = 'templates')
 
 def store_info(token):
     with open('token_list', 'a') as token_file:
